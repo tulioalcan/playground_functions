85a5d9a1e7cfc83352ab7d48453680c1d75fc5e8
Túlio Mendes <tulioalcan@gmail.com>
2022-07-26 15:22:33 -0300

    Desafio 021 feito

--- src/challenges.js
+++ src/challenges.js
@@ -7,10 +7,10 @@ function compareTrue(animal1, animal2) {
   }
 }
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = ((base * height) / 2);
+  return area;
 }
-
 // Desafio 3
 function splitSentence() {
   // seu código aqui
