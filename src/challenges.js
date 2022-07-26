fab641815cbf2d73c81a99c1244823fb5466b81a
Túlio Mendes <tulioalcan@gmail.com>
2022-07-26 16:40:52 -0300

    Desafio 05 feito

--- src/challenges.js
+++ src/challenges.js
@@ -23,8 +23,9 @@ function concatName(names) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (wins * 3) + ties
+  return pontos
 }
 
 // Desafio 6
